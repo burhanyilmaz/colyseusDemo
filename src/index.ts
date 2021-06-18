6710815d2de74590453a6a95fb58536b5e175793
Burhan Yılmaz <burhan.yilmaaz@gmail.com>
2021-06-18 13:21:00 +0300

    feat: first init

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Room, Server } from 'colyseus';
+import { createServer } from 'http';
+import express from 'express';
+
+class MyRoom extends Room {
+  onCreate() {
+    this.onMessage('up', (client, message) => {
+      client.send('up', { message, date: new Date() });
+    });
+    this.onMessage('*', (client, type, message) => {
+      console.log(client.sessionId, 'sent', type, message);
+    });
+  }
+}
+
+const port = Number(process.env.port) || 3030;
+
+const app = express();
+app.use(express.json());
+
+const gameServer = new Server({
+  server: createServer(app),
+});
+
+gameServer.define('room1', MyRoom);
+
+gameServer.listen(port);
